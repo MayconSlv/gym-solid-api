b6166f04b91b6fce58ca2cc3f11c4ec14267318e
Maycon Silva <mayconslv.contato@gmail.com>
2023-10-07 17:24:46 -0300

    Controllers & Testes E2E - Testes E2E de rotas de check-ins

---
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -0,0 +1,50 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/craete-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Check-in Metrics (e2e)', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list the total count of check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript Gym',
+        latitude: -8.0966109,
+        longitude: -34.8941198,
+      },
+    })
+
+    await prisma.checkin.createMany({
+      data: [
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/check-ins/metrics')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkIns).toEqual(2)
+  })
+})
